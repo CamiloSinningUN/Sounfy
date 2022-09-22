8f4c4e33b40d759ce57eb3fadd3eae0b2d461cde
Camilo Sinning <camilosinning.cs@gmail.com>
2022-09-21 23:55:05 -0500

    begin

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html', 'playlist.html'],
+  theme: {
+    extend: {},
+    colors: {
+      'gray': "#191919",
+      'orange': "#FF5964",
+      'white': "#FFFFFF",
+      'blue': "#35A6FF",
+      'yellow': "#FFFF00",
+      'black': "#000000", 
+      'gray-2': "#222222",
+    },
+    fontFamily: {
+      'Reem-Kufi': ['"Reem Kufi"', 'sans-serif'],
+    },
+  },
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
+}
